54a8c1ec5cc2f5bf236511706729da743789910f
nahom-23 <noahbirhanu23@gmail.com>
2024-06-18 07:36:49 +0300

    first commit

---
+++ client/quiz_app/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-medium underline text-red-900">Hello!</h1>
+    </>
+  );
+}
+
+export default App;
