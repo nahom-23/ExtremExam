11086bf35b9096e4541b362812baa73761f5c8e3
nahom-23 <noahbirhanu23@gmail.com>
2024-10-10 23:45:41 +0300

    add image limit and filtering for image only

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -9,6 +9,19 @@ const storage = new CloudinaryStorage({
     allowedFormats: ["jpg", "png", "jpeg"],
   },
 });
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true); // Allow the file
+    } else {
+      cb(new Error("Invalid file type. Only image files are allowed."), false);
+    }
+  },
+});
 
 module.exports = upload;
