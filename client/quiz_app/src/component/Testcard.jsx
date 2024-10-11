c82a09af0a66d720fcdc90036382e8a5777c8a51
nahom-23 <noahbirhanu23@gmail.com>
2024-10-11 23:01:46 +0300

    updated

--- client/quiz_app/src/component/Testcard.jsx
+++ client/quiz_app/src/component/Testcard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+
 function Testcard() {
   const [category, setCategory] = useState([]);
   useEffect(() => {
