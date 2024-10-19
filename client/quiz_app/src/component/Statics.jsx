04df201f19d0c8633efeb2ff282a0deec00ad624
nahom-23 <noahbirhanu23@gmail.com>
2024-10-20 01:44:31 +0300

    use odometer for number counter animation

--- client/quiz_app/src/component/Statics.jsx
+++ client/quiz_app/src/component/Statics.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
-import useFetchUserData from "../hooks/useFetchUserData";
-
+import Odometer from "react-odometerjs";
+import useFetch from "../hooks/useFetch";
+import "odometer/themes/odometer-theme-default.css";
 function Statics() {
-  const { userData } = useFetchUserData();
+  const { userData, questions } = useFetch();
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setCount(userData.length - 1), 2000);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [userData]);
 
   return (
     <>
@@ -11,8 +18,8 @@ function Statics() {
         <div className="max-w-6xl mx-auto my-9 flex items-center bg-green h-[200px] rounded-3xl">
           <div className="flex items-center justify-between w-[80%] mx-auto text-white flex-wrap">
             <h1 className="text-xl tracking-wide uppercase">
-              <span className="block text-6xl font-bold text-center ">
-                {userData.length - 1}+
+              <span className="block text-6xl font-bold text-center truncate">
+                <Odometer value={count} format="(.ddd),dd" />+
               </span>
               Active Users Right Now
             </h1>
@@ -24,7 +31,7 @@ function Statics() {
             </h1>
             <h1 className="text-xl tracking-wide uppercase">
               <span className="block text-6xl font-bold text-center ">
-                {userData.length}+
+                {questions.length}+
               </span>
               questions
             </h1>
