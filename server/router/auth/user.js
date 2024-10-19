afa338052089fe7cf64951c3b127251e82bf05af
nahom-23 <noahbirhanu23@gmail.com>
2024-10-19 04:16:50 +0300

    update user folder

--- server/router/auth/user.js
+++ server/router/auth/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signupUser, loginUser } = require("../controller/userController");
+const { signupUser, loginUser } = require("../../controller/userController");
 
 const router = express.Router();
 
