e8b609e8eb276959ef1cd4a2581c6db4a9afeacf
nahom-23 <noahbirhanu23@gmail.com>
2024-10-20 01:43:31 +0300

    add question fetching  to useFetch

--- client/quiz_app/src/hooks/useFetch.jsx
+++ client/quiz_app/src/hooks/useFetch.jsx
@@ -3,9 +3,17 @@ import axios from "axios";
 
 const useFetch = () => {
   const [categories, setCategories] = useState([]);
+  const [userData, setUserData] = useState([]);
+  const [questions, SetQuestions] = useState([]);
 
   useEffect(() => {
     fetchCategories();
+  }, []);
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+  useEffect(() => {
+    fetchQuestions();
   }, []);
 
   const fetchCategories = async () => {
@@ -16,8 +24,26 @@ const useFetch = () => {
       console.log(error);
     }
   };
+  const fetchQuestions = async () => {
+    try {
+      const res = await axios.get("/api/questions");
+      SetQuestions(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchUserData = async () => {
+    try {
+      const res = await axios.get("/api/users");
+      if (res.data) {
+        setUserData(res.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  return { categories, fetchCategories };
+  return { categories, fetchCategories, userData, questions };
 };
 
 export default useFetch;
