24c5ef0b40a9115a66533a42bc942e913f124fb6
nahom-23 <noahbirhanu23@gmail.com>
2024-10-19 04:11:03 +0300

    update the tailwind config

--- client/quiz_app/tailwind.config.js
+++ client/quiz_app/tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
+      backgroundImage: {
+        "contact-us": "url('/assets/civil.png')",
+      },
+      backgroundSize: {
+        16: "75%",
+      },
       animation: {
         "my-text": "my-text 1s linear",
         "my-images": "my-images 1s linear",
